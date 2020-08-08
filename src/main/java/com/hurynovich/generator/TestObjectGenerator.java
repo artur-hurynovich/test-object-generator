9047ee8762eb64c2f877fb368fddb8c09d06c336
Artur Hurynovich <artur.hurynovich@gmail.com>
2020-08-08 20:01:53 +0300

    Use Spring's ReflectionUtils

--- src/main/java/com/hurynovich/generator/TestObjectGenerator.java
+++ src/main/java/com/hurynovich/generator/TestObjectGenerator.java
@@ -1,7 +1,11 @@
 package com.hurynovich.generator;
 
+import java.util.Collection;
+
 public interface TestObjectGenerator {
 
 	<T> T generate(Class<T> objectClass);
+
+	<T> T generate(Class<T> objectClass, Collection<String> ignoreFields);
 
 }
--- src/main/java/com/hurynovich/generator/impl/GeneralTestObjectGenerator.java
+++ src/main/java/com/hurynovich/generator/impl/GeneralTestObjectGenerator.java
@@ -10,51 +10,96 @@ import com.hurynovich.exception.TestObjectGeneratorException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Collections;
 
 public class GeneralTestObjectGenerator implements TestObjectGenerator {
 
+	private static final Log LOGGER = LogFactory.getLog(GeneralTestObjectGenerator.class);
+
+	private static final String SETTER_PREFIX = "set";
+
 	private final TestObjectClassValidator validator = TestObjectClassValidatorFactory.build();
 
-	public <T> T generate(final Class<T> testObjectClass) {
-		validator.validate(testObjectClass);
+	@Override
+	public <T> T generate(final Class<T> objectClass) {
+		return generate(objectClass, Collections.emptyList());
+	}
 
-		final T testObject = instantiate(testObjectClass);
+	@Override
+	public <T> T generate(final Class<T> objectClass, final Collection<String> ignoreFields) {
+		validator.validate(objectClass);
 
-		final List<Field> fields = Arrays.asList(testObjectClass.getDeclaredFields());
-		fields.forEach(field -> {
-			field.setAccessible(true);
+		// recursive
+		// if objectClass is String or boolean or so on?
+		final T testObject = instantiate(objectClass);
 
-			final Class<?> fieldType = field.getType();
-
-			try {
-				if (fieldType.isPrimitive()) {
-					final PrimitiveTypeGenerator<? extends Serializable> primitiveTypeGenerator =
-							PrimitiveTypeGeneratorFactory.build(fieldType);
-
-					if (primitiveTypeGenerator != null) {
-						field.set(testObject, primitiveTypeGenerator.generate());
-					}
-				} else {
-					field.set(testObject, generate(fieldType));
-				}
-			} catch (final IllegalAccessException e) {
-				throw new TestObjectGeneratorException("Failed to set value for field of class '" + fieldType +
-						"' with name '" + field.getName() + "'", e);
+		ReflectionUtils.doWithFields(objectClass, field -> {
+			if (!ignoreFields.contains(field.getName())) {
+				processSetField(field, testObject);
 			}
 		});
 
 		return testObject;
 	}
 
-	private <T> T instantiate(final Class<T> testObjectClass) {
+	private <T> T instantiate(final Class<T> objectClass) {
 		try {
-			return testObjectClass.getDeclaredConstructor().newInstance();
-		} catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+			return ReflectionUtils.accessibleConstructor(objectClass).newInstance();
+		} catch (final InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
 			throw new TestObjectGeneratorException("Failed to instantiate test object of class '" +
-					testObjectClass + "'", e);
+					objectClass + "'", e);
 		}
+	}
+
+	private <T> void processSetField(final Field field, final T object) {
+		final Class<?> fieldType = field.getType();
+
+		if (fieldType.isPrimitive() || fieldType.equals(String.class)) {
+			final DefaultTypeGenerator<Serializable> defaultTypeGenerator =
+					DefaultTypeGeneratorFactory.build(fieldType);
+
+			final Class<?> objectClass = object.getClass();
+
+			if (defaultTypeGenerator != null) {
+				final Method setter = findStandardSetterForField(field, objectClass);
+
+				if (setter != null) {
+					ReflectionUtils.invokeMethod(setter, object, defaultTypeGenerator.generate());
+				} else {
+					LOGGER.warn("No standard setter found for field of type '" + fieldType +
+							"' and name '" + field.getName() + "' in class '" + objectClass.getName() + "'");
+				}
+			}
+		}
+
+		// array, collection (including map); Generics???
+	}
+
+	private <T> Method findStandardSetterForField(final Field field, final Class<T> clazz) {
+		final String setterName = resolveSetterName(field);
+
+		final Method setter = ReflectionUtils.findMethod(clazz, setterName, field.getType());
+
+		if (setter != null && isPublic(setter)) {
+			return setter;
+		} else {
+			return null;
+		}
+	}
+
+	private String resolveSetterName(final Field field) {
+		final String fieldName = field.getName();
+
+		return SETTER_PREFIX + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+	}
+
+	private boolean isPublic(final Method method) {
+		final int modifiers = method.getModifiers();
+
+		return Modifier.isPublic(modifiers);
 	}
 
 }
